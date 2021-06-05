27b435b1bb2175e9f679dc204fbc092dd93074fc
sadikRizvi31 <63736029+sadikRizvi31@users.noreply.github.com>
2021-06-05 13:17:11 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1>JavaScript and ReactJS</h1>
+        <p>
+          It was an Good session to give Headstart Towards React. Shaurya Sinha
+          sir was Amazing while teaching with their own and industrial
+          perspective. Thank Your Shape AI.
+        </p>
+      </div>
+      <div className="note" style={{ background: "#b1cbbb" }}>
+        <h1>Python & Machine Learning</h1>
+        <p>
+          It was an Good session to give Headstart Towards Machine Learning.
+          Shaurya Sinha sir was Amazing while teaching with their own and
+          industrial perspective. Thank Your Shape AI.
+        </p>
+      </div>
+      <div className="note" style={{ background: "#667292" }}>
+        <h1>DevOps</h1>
+        <p>
+          It was an Good session to give Headstart Towards DevOps. Shaurya Sinha
+          sir was Amazing while teaching with their own and industrial
+          perspective. Thank Your Shape AI.
+        </p>
+      </div>
+      <div className="note" style={{ background: "#87bdd8" }}>
+        <h1>Python With Django</h1>
+        <p>
+          It was an Good session to give Headstart Towards Framework. Shaurya
+          Sinha sir was Amazing while teaching with their own and industrial
+          perspective. Thank Your Shape AI.
+        </p>
+      </div>
+      <div className="note" style={{ background: "#ff6f69" }}>
+        <h1>Blockchain</h1>
+        <p>
+          It was an Good session to give Headstart Towards Blockchain. Shaurya
+          Sinha sir was Amazing while teaching with their own and industrial
+          perspective. Thank Your Shape AI.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Note;
